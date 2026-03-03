29d55899d7a2b763f99b32b7fcd79120ff9e5f3e
Ravikumar-code-maker <ravikumarchinthapally@gmail.com>
2026-03-03 21:36:09 +0530

    Update backend.tf

--- terraform-org-level/backend.tf
+++ terraform-org-level/backend.tf
@@ -1,8 +1,9 @@
 terraform {
   backend "gcs" {
-    bucket  = "my-terraform-state-dev"
+    bucket  = "my-terraform-state-dev1"
     prefix  = "terraform/state"
     project = var.project_id
   }
 }
+
 
