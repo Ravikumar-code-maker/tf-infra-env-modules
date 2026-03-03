be48d263b6e3dda3295522afda8f84362b74f545
Ravikumar-code-maker <ravikumarchinthapally@gmail.com>
2026-03-03 15:22:05 +0530

    Update main.tf

---
+++ terraform-org-level/modules/bigquery/main.tf
@@ -0,0 +1,13 @@
+resource "google_bigquery_dataset" "datasets" {
+  for_each   = var.datasets
+  dataset_id = each.key
+  location   = each.value.location
+}
+
+resource "google_bigquery_table" "tables" {
+  for_each = { for ds_name, ds in var.datasets : ds_name => ds.tables... }
+
+  dataset_is = each.keyn 
+  table_id   = each.value
+  schema     = file("schema.json")
+}
