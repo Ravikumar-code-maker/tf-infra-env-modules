1defc6d283e9e883c9ad6ffaf9d8d80ada833331
Ravikumar-code-maker <ravikumarchinthapally@gmail.com>
2026-03-03 23:24:37 +0530

    Update variables.tf

--- terraform-org-level/modules/gcs/variables.tf
+++ terraform-org-level/modules/gcs/variables.tf
@@ -1,9 +1,13 @@
-variable "bucket_name" {
+variable "gcs_bucket_name" {
   type    = list(string)
+  default = ["bucket-dev","bucket-test","bucket-prod"]
+
 }
 
 variable "region" {
   type    = string
+  default = "US"
 }
+
 
 
